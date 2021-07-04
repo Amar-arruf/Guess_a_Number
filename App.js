6067320103fe23ed1536c383829e8e239f6d60b9
Amar Maruf <amarmaruf2403@gmail.com>
2021-07-04 20:20:18 +0700

    adding custom components Header 2

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
+import Header from './components/Header';
 export default function App() {
   return (
     <View style={styles.screen}>
--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import React from 'rect';
+import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const Header = props => {
