b2b95ede65892216b745ae4456c80f42415d8e7b
Amar Maruf <amarmaruf2403@gmail.com>
2021-07-13 08:58:06 +0700

    Finishing responsive pada layar GameOverScreen

--- screen/GameOVerScreen.js
+++ screen/GameOVerScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import { View, Text, StyleSheet, Button, Image, Dimensions, ScrollView } from 'react-native';
 
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
@@ -7,6 +7,7 @@ import Color from '../constants/Color';
 
 const GameOverScreen = props => {
   return (
+    <ScrollView>
     <View style={styles.screen}>
       <TitleText>The Game is Over!</TitleText>
       <View style={styles.imageContainer}>
@@ -29,6 +30,7 @@ const GameOverScreen = props => {
       </View>
       <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
+    </ScrollView>
   );
 };
 
@@ -36,16 +38,17 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    paddingVertical: 10
   },
   imageContainer: {
-    width: 300,
-    height: 300,
-    borderRadius: 150,
+    width: Dimensions.get('window').width * 0.7,
+    height: Dimensions.get('window').width * 0.7,
+    borderRadius: (Dimensions.get('window').width * 0.7) / 2,
     borderWidth: 3,
     borderColor: 'black',
     overflow: 'hidden',
-    marginVertical: 30
+    marginVertical: Dimensions.get ('window').height / 30
   },
   image: {
     width: '100%',
@@ -53,12 +56,11 @@ const styles = StyleSheet.create({
   },
   resultContainer: {
     marginHorizontal: 30,
-    marginVertical: 15,
-    textAlign:'center'
+    marginVertical:Dimensions.get('window').height / 60
   },
   resultText: {
     textAlign: 'center',
-    fontSize: 20
+    fontSize: Dimensions.get('window').height < 400 ? 16 : 20
   },
   highlight: {
     color: Color.primary,
