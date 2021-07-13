e07d9a0ec2720eefcdd926ccb1a6f54c27f8d9a7
Amar Maruf <amarmaruf2403@gmail.com>
2021-07-13 09:47:44 +0700

    membuat custom button bergantung pada Platform OS

--- components/Header.js
+++ components/Header.js
@@ -1,27 +1,38 @@
 import React from 'react';
-import { View, Text, StyleSheet, Platform } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 import Color from '../constants/Color';
 import TitleText from './TitleText';
 
 const Header = props => {
   return (
-    <View style={styles.header}>
+    <View style={{
+      ...styles.headerBase, 
+      ...Platform.select({
+      ios : styles.headerIOS,
+      android : styles.headerAndroid
+      })
+    }}>
       <TitleText style={styles.headerTitle}>{props.title}</TitleText>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  header: {
+  headerBase: {
     width: '100%',
     height: 90,
     paddingTop: 36,
-    backgroundColor: Platform.OS === 'android' ? Color.primary : 'white',
     alignItems: 'center',
-    borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent', 
-    borderBottomWidth: Platform.OS === 'ios' ? 1 : 0,
     justifyContent: 'center'
+  },
+  headerIOS : {
+    backgroundColor: 'white',
+    borderBottomColor:'#ccc', 
+    borderBottomWidth:  1 ,
+  },
+  headerAndroid : {
+    backgroundColor: Color.primary,
   },
   headerTitle: {
     color: Platform.OS === 'ios' ? Color.primary : 'white',
